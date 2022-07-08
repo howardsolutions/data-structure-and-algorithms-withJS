173c62243d50fca12e5ed4c50effa183578b34de
thinhphungdev <thinhphung.contact@gmail.com>
2022-07-08 22:59:17 +0700

    feat: fibonacci

--- Recursion.js
+++ Recursion.js
@@ -18,7 +18,7 @@
 // RECURSION QUESTION //
 ////////////////////////////////////////////////////////////////////////
 
-// Write two functions that finds the factorial of any number. One should use recursive, the other should just use a for loop
+// 1. Write two functions that finds the factorial of any number. One should use recursive, the other should just use a for loop
 function findFactorialIterative(n) {
   if (n === 0 || n === 1) return 1;
 
@@ -43,13 +43,44 @@ function findFactorialIterative(n) {
 // space: O(1)
 
 function findFactorialRecursive(n) {
-    // base case
-    if (n === 0) return 'Please enter valid number';
-    if (n === 1) return 1;
-    //recursive case
-    return n * findFactorialRecursive(n - 1);
+  // base case
+  if (n === 0) return "Please enter valid number";
+  if (n === 1) return 1;
+  //recursive case
+  return n * findFactorialRecursive(n - 1);
 }
 // console.log(findFactorialRecursive(4))
 // f(4) = 4 * f(3) = 4 * 3 * f(2) = 4*3*2*f(1) = 4*3*2*1 = 24;
 // Time: O(n)
 // Space: O(1)
+
+// 2. Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+//For example: fibonacciRecursive(6) should return 8
+
+function fibonacciIterative(n) {
+  let arr = [0, 1];
+
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+
+  return arr[n];
+}
+// console.log(fibonacciIterative(3));
+
+function fibonacciRecursive(n) {
+  // base case
+  if (n < 2) return n;
+
+  // recursive case
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+console.log(fibonacciRecursive(4)); // 2
+
+// f(3) = f(2) + f(1) = f(2) + 1 = f(1) + f(0) + 1 = 1 + 1 + 0 = 2
+// f(n) = f(n-1) + f(n-2)
