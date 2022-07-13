20d367b9c3104323ce43bfbbf73e662b6073248c
thinhphungdev <thinhphung.contact@gmail.com>
2022-07-13 17:11:01 +0700

    feat: selection sort solution

--- Sorting.js
+++ Sorting.js
@@ -3,22 +3,52 @@
 // ELEMENTARY SORT
 ///////////////////////////////////////
 
-// Bubble Sort: Bubble the highest value 
+// Bubble Sort: Bubble the highest value
 function bubbleSort(array) {
-    if (array.length === 0) return 'Please enter valid array';
-    const length = array.length;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length; j++) {
-        if (array[j] > array[j + 1]) {
-          // swap
-          const temp = array[j + 1];
-          array[j + 1] = array[j]
-          array[j] = temp;
-        }
+  if (array.length === 0) return "Please enter valid array";
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (array[j] > array[j + 1]) {
+        // swap
+        const temp = array[j + 1];
+        array[j + 1] = array[j];
+        array[j] = temp;
       }
     }
-  
-    return array;
+  }
+
+  return array;
 }
 
+/* Complexity analyst:
+ Worst and Avarage case: O(n^2), Space: O(1)
+ Best: when array already sorted: O(n) time 
+*/
+
 // Selection Sort: Select the smallest value through interations
+function selectionSort(array) {
+  if (array.length === 0) return "Please enter valid array";
+  const length = array.length;
+
+  for (let i = 0; i < length; i++) {
+    let smallestIdx = i;
+
+    for (let j = i + 1; j < length; j++) {
+      if (array[smallestIdx] > array[j]) smallestIdx = j;
+      // looping until find the smallest idx inside the interation
+    }
+    // and then swap the number
+    swap(array, i, smallestIdx);
+  }
+
+  return array;
+}
+
+function swap(array, i, j) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+// Time: O(n^2) - exponential,
