32a66fde2e8c0f92d74d7f2b343544fb20b30c67
thinhphungdev <thinhphung.contact@gmail.com>
2022-07-13 16:58:26 +0700

    feat: buble sort solution

--- Sorting.js
+++ Sorting.js
@@ -4,5 +4,21 @@
 ///////////////////////////////////////
 
 // Bubble Sort: Bubble the highest value 
+function bubbleSort(array) {
+    if (array.length === 0) return 'Please enter valid array';
+    const length = array.length;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        if (array[j] > array[j + 1]) {
+          // swap
+          const temp = array[j + 1];
+          array[j + 1] = array[j]
+          array[j] = temp;
+        }
+      }
+    }
+  
+    return array;
+}
 
 // Selection Sort: Select the smallest value through interations
