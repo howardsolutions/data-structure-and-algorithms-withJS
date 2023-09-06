c27b4b7e05e5458cec0b91a174a493fee9eb09d2
howardphungdev <phungthinh0946519356@gmail.com>
2023-09-07 06:28:32 +0700

    graph: connected component count

--- Graph/question.js
+++ Graph/question.js
@@ -95,6 +95,7 @@ const hasPathBFS = (graph, src, dst) => {
 ////////////////////////////////////////////
 /*
  Undirected path: Time: O(e), Space: O(n)
+ cyclic (graph contains at least one cycle) & undirected
 */
 ////////////////////////////////////////////
 
@@ -141,3 +142,46 @@ function buildGraph(edges) {
 }
 
 // console.log(undirectedPath(edges, "i", "l"));
+////////////////////////////////////////////
+/*
+ (connected components) count
+ take in an adjecency list represent an undirected graph
+*/
+////////////////////////////////////////////
+
+const graphData = {
+  3: [],
+  4: [6],
+  6: [4, 5, 7, 8],
+  8: [6],
+  7: [6],
+  5: [6],
+  1: [2],
+  2: [1],
+};
+
+const connectedComponentsCount = (graph) => {
+  const visited = new Set();
+  let count = 0;
+  for (let node in graph) {
+    if (explore(graph, node, visited) === true) {
+      count += 1;
+    }
+  }
+
+  return count;
+};
+
+const explore = (graph, current, visited) => {
+  if (visited.has(String(current))) return false;
+
+  visited.add(String(current));
+
+  for (let neighbor of graph[current]) {
+    explore(graph, neighbor, visited);
+  }
+
+  return true;
+};
+
+// console.log(connectedComponentsCount(graphData));
